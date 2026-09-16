b544d7dc5c38a2c808b524719b74cfaf63ac2977
jashwanthalla <jaswanth.alla23@gmail.com>
2026-09-16 17:07:47 +0530

    commit

--- sql/sql commands.sql
+++ sql/sql commands.sql
@@ -22,3 +22,10 @@ select id,customer_id,shipper,freight from orders where freight>100;
 select * from products;
 -- comments
 select id,product_name,category,unit_price from products where category="Condiments" and discontinued=0;
+
+select * from orders;
+select id,customer_id,shipper,freight,order_date from orders where order_date between '2020-01-01' and '2020-12-31';
+
+select * from customers;
+
+select id,phone from customers where country="USA" or country="Germany";
